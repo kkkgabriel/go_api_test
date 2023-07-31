cdc5aa461eeecf0081ed05e36098080c7babf801
Raphael Koh <raphael@Raphaels-Mac-mini.local>
2023-08-01 02:17:05 +0800

    working

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,14 @@
 FROM golang
 
-ENV GIN_MODE=release
-ENV PORT=3004
-
-#RUN apk update && apk add --no-cache git
+EXPOSE 8080
 
 WORKDIR $GOPATH/src/mypackage/myapp/
+
 COPY . .
 
-RUN go get -d -v
+RUN go mod tidy
 
 RUN go build -o /go/bin/go_api
 
-EXPOSE $PORT
-EXPOSE 8080
 
 ENTRYPOINT ["/go/bin/go_api"]
--- main.go
+++ main.go
@@ -13,15 +13,20 @@ type album struct {
 
 func main() {
     router := gin.Default()
+    router.GET("/", home)
     router.GET("/albums", getAlbums)
     router.POST("albums", postAlbums)
-    router.Run("localhost:8080")
+    router.Run()
 }
 
 var albums = []album{
     {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
     {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
     {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func home(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, "this works")
 }
 
 func getAlbums(c *gin.Context) {
